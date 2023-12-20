2a1015da4b18bcfd1387ef0b3f4b74a6ddf6c154
hathanhhai <33385714+hathanhhai@users.noreply.github.com>
2023-12-20 22:39:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const AUTH_TOKEN = "2qsVznjhF54E4P7QAHnz8H71tFQk4XoBXO9BnJLv";
 const ZONE_ID = "49d92e256ae53d3ac2d6650e4ecf3a7b";
 const DOMAIN = "congcucuatoi.com";
 
-const PORT = 500;
+const PORT = 1001;
 //----------
 const Axios = axios.create({
   timeout: 1000,
@@ -17,11 +17,12 @@ const Axios = axios.create({
 app.get("/", async (req, res) => {
   try {
     var publicIP = await Axios.get(`https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records?name=${DOMAIN}&type=A`);
+    console.log('publicIP: ', publicIP);
     var IP = publicIP.data.result[0].content;
     res.render('ip',{ip:IP})
     // res.send(IP);
   } catch (err) {
-    return res.send(JSON.stringify(err));
+    return res.send({message:err});
   }
 });
 
